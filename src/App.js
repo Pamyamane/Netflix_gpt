701095e8b2abe117ec0d7748dde7d56a0c463dac
Pamyamane <pramodmane043@gmail.com>
2025-02-01 12:50:04 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl font-bold underline text-blue-500">
+      Hello, Tailwind CSS!
+    </div>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Click Me
+</button>
+
+    </div>
+  );
+}
+
+export default App;
